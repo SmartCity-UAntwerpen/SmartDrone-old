9993bab138e154a91bb788129f0742bdfb380f5b
KlaasDeNys <klaas.denys@outlook.com>
2017-04-27 17:39:34 +0200

    Start connection with Thomas and Nick

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
 from drone import Drone
+import socket
+import sys
+class godfather:
+    def __init__(self):
+        self.list = []
+        self.s=0
+        self.init_socket()
+        self.wait_for_instruction()
 
-list = []
+    def init_socket(self):
+        HOST = '192.168.43.111'# Symbolic name, meaning all available interfaces
+        PORT = 8888 # Arbitrary non-privileged port
 
-def create_drone():
-    list.append(Drone())
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print 'Socket created'
+        try:
+            self.s.bind((HOST, PORT))
+        except socket.error as msg:
+            print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+            sys.exit()
+        print 'Socket bind complete'
 
-def remove_drone(id):
-    for drone in list:
-        if (drone.id==id):
-            print ("Drone "+ str(drone.id)+" removed")
-            list.remove(drone)
+        #Start listening on socket
+        self.s.listen(10)
+        print 'Socket now listening'
 
-create_drone()
-create_drone()
-remove_drone(2)
-for drone in list:
-    print(drone)
+
+
+    def create_drone(self):
+        self.list.append(Drone(0,0,0))
+
+    def remove_drone(self, id):
+        for drone in self.list:
+            if (drone.id==id):
+                print ("Drone "+ str(drone.id)+" removed")
+                self.list.remove(drone)
+
+    def wait_for_instruction(self):
+    #now keep talking with the client
+        while (1):
+            #wait to accept a connection - blocking call
+            conn, addr = self.s.accept()
+            print 'Connected with ' + addr[0] + ':' + str(addr[1])
+            print 'Data:'
+            data= conn.recv(1024)
+            print data
+            data = data.split(" ")
+            if data[0]=="create":
+                self.create_drone()
+                print "create"
+        self.s.close()
+
+godfather()
