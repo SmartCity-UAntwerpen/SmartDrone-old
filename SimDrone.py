aedc337ad2cc7081a97c489e66776a115617d17f
Reinout <reinout.VanHille@student.uantwerpen.be>
2017-05-04 11:30:54 +0200

    split drone - simdrone using inheritance

---
+++ SimDrone.py
@@ -0,0 +1,148 @@
+from drone import *
+import math
+
+# Simulation values
+speed_takeoff = 0.5
+speed_landing = 0.5
+fly_height = 4  # drone wil go to this height during takeoff sequence
+speed_horizontal = 5
+
+
+class SimDrone(Drone):
+
+    def __init__(self):
+        self.init_x = 0
+        self.init_y = 0
+        self.init_z = 0
+        self.is_set = False
+        super(SimDrone, self).__init__()
+
+    # set the initial x/y/z coordinates
+    def set(self, x, y, z):
+        if self.running:
+            return "NACK\n"
+
+        else:
+            self.init_x = x
+            self.init_y = y
+            self.init_z = z
+
+            self.x = x
+            self.y = y
+            self.z = z
+            self.is_set = True
+            return "ACK\n"
+
+    # reset current location
+    def _reset(self):
+        self.x = self.init_x
+        self.y = self.init_y
+        self.z = self.init_z
+
+    # stop the drone
+    def stop(self):
+        if self.running:
+            self.running = False
+            self._unregister_job()
+            self._unregister_pos()
+            return "ACK\n"
+        else:
+            return "NACK\n"
+
+    # restart drone
+    def restart(self):
+        self.stop()
+        self._reset()
+        self.run()
+
+    # start the drone
+    def run(self):
+        if self.running or not self.is_set:  # don't start another thread if already running
+            return "NACK\n"
+        else:
+            super(SimDrone, self).run()
+            return "ACK\n"
+
+    # delete drone
+    def kill(self):
+        self.running = False
+        self._unregister_pos()
+        self._unregister_job()
+        del self
+
+    # flying
+    def _sim_fly(self, speed, distance, dist_x, dist_y):
+        if dist_x == 0:  # catch divide by zero issues
+            a = math.pi / 2
+        else:
+            a = math.atan(dist_y / dist_x)  # determine fly angle
+        if dist_x > 0:
+            sx = 1
+        else:
+            sx = -1
+        if dist_y > 0:
+            sy = 1
+        else:
+            sy = -1
+        remainder = distance % speed
+        distance -= remainder
+        traveled = 0
+        while traveled != distance:
+            traveled += speed
+            self.x += sx * speed * abs(math.cos(a))
+            self.y += sy * speed * abs(math.sin(a))
+            time.sleep(1)
+
+        self.x += sx * remainder * abs(math.cos(a))
+        self.y += sy * remainder * abs(math.sin(a))
+
+    # takeoff + landing simulation
+    def _sim_vertical(self, speed, height):
+        dist_z = height - self.z
+        remainder = dist_z % speed
+        dist_z -= remainder
+        if dist_z > 0:
+            lift = 1
+        else:
+            lift = -1
+        traveled = 0
+        while traveled != dist_z:
+            traveled += lift * speed
+            self.z += lift * speed
+            time.sleep(1)
+        self.z += lift * remainder
+
+    # fly drone
+    def _fly(self, coord):
+        self.job = True
+        distance = SimDrone._calc_dist(self.x, self.y, coord[0], coord[1])
+        dist_x = coord[0]-self.x
+        dist_y = coord[1]-self.y
+
+        self._sim_vertical(speed_takeoff, fly_height)  # takeoff to fly height
+        self._sim_fly(speed_horizontal, distance, dist_x, dist_y)  # cover distance in x & y direction
+        self._sim_vertical(speed_landing, coord[2])  # move to end height
+        self.job = False
+
+    # calculate the weight for a job
+    def _calc_weight(self, client, userdata, msg):
+        data = str(msg.payload).split(",")
+        data = map(float, data)
+        coorda = (data[0], data[1], data[2])
+        coordb = (data[3], data[4], data[5])
+
+        #  flytime = time to reach initial point + time to reach end point
+        flytime = abs(fly_height-self.z)/speed_takeoff
+        flytime += abs(fly_height-coorda[2])/speed_landing
+        flytime += SimDrone._calc_dist(self.x, self.y, coorda[0], coordb[1])/speed_horizontal
+
+        flytime += abs(coorda[2]-fly_height)/speed_takeoff
+        flytime += abs(fly_height-coordb[2])/speed_landing
+        flytime += SimDrone._calc_dist(coorda[0], coorda[1], coordb[0], coordb[1])
+
+    # calculate distance between points
+    @staticmethod
+    def _calc_dist(x1, y1, x2, y2):
+        a = pow(x2 - x1, 2)
+        b = pow(y2 - y1, 2)
+        return math.sqrt(a+b)
--- drone.py
+++ drone.py
@@ -1,112 +1,46 @@
 import paho.mqtt.client as mqttclient
-import math
-import requests
 import thread
 import time
 from random import randint
 
-simulation = True
 
-speed_takeoff = 0.5
-speed_landing = 0.5
-fly_height = 4  # drone wil go to this height during takeoff sequence
-speed_horizontal = 5
+class Drone(object):
 
-
-class Drone:
-
-    # creates a drone - connects to required topics
+    # creates a drone
     def __init__(self):
         self.id = Drone._get_id()
         self.job_client = mqttclient.Client()
         self.pos_client = mqttclient.Client()
-        self.app_listener = mqttclient.Client()
-        self.app_publisher = mqttclient.Client()
         self.x = 0
         self.y = 0
         self.z = 0
-        self.init_x = 0
-        self.init_y = 0
-        self.init_z = 0
-        self.is_set = False
-        self.running = False
         self.job = False
-
-    # set the initial x/y/z coordinates
-    def set(self, x, y, z):
-        if self.running:
-            return "NACK"
-
-        else:
-            self.init_x = x
-            self.init_y = y
-            self.init_z = z
-
-            self.x = x
-            self.y = y
-            self.z = z
-            self.is_set = True
-            return "ACK"
+        self.running = False
+        self.run()
 
     # start the drone
     def run(self):
-        if self.running or not self.is_set:  # don't start another thread if already running
-            return "NACK"
-        else:
-            self.running = True
-            self._reg_jobs()
-            self._reg_pos()
-            self._reg_app_listener()
-            self._reg_app_publisher()
-            try:
-                thread.start_new_thread(self._pos_loop, (self.id,))
-            except thread.error as e:
-                print(e)
-                return "NACK"
-            return "ACK"
-
-    # reset current location
-    def _reset(self):
-        self.x = self.init_x
-        self.y = self.init_y
-        self.z = self.init_z
-
-    # stop the drone
-    def stop(self):
-        if self.running:
-            self.running = False
-            self._unregister_app_listener()
-            self._unregister_app_publisher()
-            self._unregister_job()
-            self._unregister_pos()
-            self._reset()
-            return "ACK"
-        else:
-            return "NACK"
-
-    # restart drone
-    def restart(self):
-        self.stop()
-        self._reset()
-        self.run()
+        self._reg_jobs()
+        self._reg_pos()
+        self.running = True
+        try:
+            thread.start_new_thread(self._pos_loop, (self.id,))
+        except thread.error as e:
+            print(e)
 
     # get an id from the server
     @staticmethod
     def _get_id():
         a = randint(0, 99)
-        print (a)
         return a
-        # return int(requests.get("http://newDrone").text)  # TODO send home location to server ?
+        # return int(requests.get("http://newDrone").text)  # TODO fix ID request
 
-    # delete drone
-    def kill(self):
-        del self
-
-    # helper function for creating drone mqtt client
+    # helper function for creating drone mqtt clients - note IDs must be unique -> marker
     # set internal to avoid confusion
     @staticmethod
-    def _create_client(_id):
-        client = mqttclient.Client("Drone " + str(_id))
+    def _create_client(_id, marker):
+        client = mqttclient.Client("Drone " + str(_id)+str(marker))
+        client.username_pw_set("root", "smartcity")
         client.connect("smartcity-ua.ddns.net", 1883, 60)
         return client
 
@@ -118,25 +52,14 @@ class Drone:
 
     # register to the job receiving channel
     def _reg_jobs(self):
-        self.job_client = self._create_client(self.id)
+        self.job_client = self._create_client(self.id, "job")
         self.job_client.subscribe("job/"+str(self.id))
         self.job_client.on_message = self._job  # register job execution function
         self.job_client.loop_start()
 
     # register to the position publishing channel
     def _reg_pos(self):
-        self.pos_client = self._create_client(self.id)
-
-    # register to the app channel
-    def _reg_app_listener(self):
-        self.app_listener = self._create_client(self.id)
-        self.app_listener.subscribe("app")
-        self.app_listener.on_message = self._calc_weight
-        self.app_listener.loop_start()
-
-    # register to the response channel
-    def _reg_app_publisher(self):
-        self.app_publisher = self._create_client(self.id)
+        self.pos_client = self._create_client(self.id, "pos")
 
     # unregister position channel
     def _unregister_pos(self):
@@ -147,93 +70,16 @@ class Drone:
         self.job_client.disconnect()
         self.job_client.loop_stop()
 
-    # unregister app channel listener
-    def _unregister_app_listener(self):
-        self.app_listener.disconnect()
-        self.app_listener.loop_stop()
-
-    # unregister app channel publisher
-    def _unregister_app_publisher(self):
-        self.app_publisher.disconnect()
-
-    # calculate the weight for a job
-    def _calc_weight(self, client, userdata, msg):
-        data = str(msg.payload).split(",")
-        coorda = self._get_coord(data[0])
-        coordb = self._get_coord(data[1])
-
-        flytime = abs(fly_height-self.z)/speed_takeoff
-        flytime += abs(fly_height-coorda[2])/speed_landing
-        flytime += Drone._calc_dist(self.x, self.y, coorda[0], coordb[1])/speed_horizontal
-
-        flytime += abs(coorda[2]-fly_height)/speed_takeoff
-        flytime += abs(fly_height-coordb[2])/speed_landing
-        flytime += Drone._calc_dist(coorda[0], coorda[1], coordb[0], coordb[1])
-        self.app_publisher.publish("app/time/"+str(flytime))
-
-    # get coordinates from server for point X
-    def _get_coord(self,x):
-        s_coord = requests.get("server/coords/"+str(x)).text
-        coord = s_coord.split(",")
-        return map(float, coord)
-
     # simulate job execution behavior
     # signature must match expected on_message signature
     def _job(self, client, userdata, msg):
-        coord = self._get_coord(str(msg.payload))
-        distance = Drone._calc_dist(self.x, self.y, coord[0], coord[1])
-        dist_x = coord[0]-self.x
-        dist_y = coord[1]-self.y
-        if simulation:
-            self._sim_vertical(speed_takeoff, fly_height)  # takeoff to fly height
-            self._sim_fly(speed_horizontal, distance, dist_x, dist_y)  # cover distance in x & y direction
-            self._sim_vertical(speed_landing, coord[2])  # move to end height
+        coord = str(msg.payload).split(",")
+        coord = map(float, coord)
+        if not self.job:  # don't handle new job if job ongoing
+            thread.start_new_thread(self._fly, (coord,))
 
-    # calculate distance between points
-    @staticmethod
-    def _calc_dist(x1, y1, x2, y2):
-        a = pow(x2 - x1, 2)
-        b = pow(y2 - y1, 2)
-        return math.sqrt(a+b)
-
-    # takeoff + landing simulation
-    def _sim_vertical(self, speed, height):
-        dist_z = height-self.z
-        remainder = dist_z % speed
-        dist_z -= remainder
-        if dist_z > 0:
-            lift = 1
-        else:
-            lift = -1
-        traveled = 0
-        while traveled != dist_z:
-            traveled += lift*speed
-            self.z += lift*speed
-            time.sleep(1)
-        self.z += lift*remainder
-
-    # flying
-    def _sim_fly(self, speed, distance, dist_x, dist_y):
-        if dist_x == 0:  # catch divide by zero issues
-            a = math.pi/2
-        else:
-            a = math.atan(dist_y/dist_x)  # determine fly angle
-        if dist_x > 0:
-            sx = 1
-        else:
-            sx = -1
-        if dist_y > 0:
-            sy = 1
-        else:
-            sy = -1
-        remainder = distance % speed
-        distance -= remainder
-        traveled = 0
-        while traveled != distance:
-            traveled += speed
-            self.x += sx*speed*abs(math.cos(a))
-            self.y += sy*speed*abs(math.sin(a))
-            time.sleep(1)
-
-        self.x += sx*remainder*abs(math.cos(a))
-        self.y += sy*remainder*abs(math.sin(a))
+    # fly to coord
+    def _fly(self, coord):
+        self.job = True
+        pass  # todo implement
+        self.job = False
