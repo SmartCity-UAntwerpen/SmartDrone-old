d77ec0c8390971589309ceddce081e3edf80c7d1
KlaasDeNys <klaas.denys@outlook.com>
2017-05-17 17:53:05 +0200

    posall make json

--- coreinterface.py
+++ coreinterface.py
@@ -1,6 +1,7 @@
 from random import randint
 from waypoint import waypoint
 import cherrypy
+import json
 
 from droneparameters import DroneParameters
 import requests
@@ -17,7 +18,7 @@ class coreinterface():
         cherrypy.tree.mount(calcWeight(), '/calcWeight', {'/': {'tools.gzip.on': True}})
         cherrypy.tree.mount(posAll(self.id_droneparam),'/posall', {'/': {'tools.gzip.on': True}})
         cherrypy.tree.mount(job(self.id_droneparam, self.mqtt_client),'/job', {'/': {'tools.gzip.on': True}})
-        cherrypy.tree.mount(advertise(), '/advertise', {'/': {'tools.gzip.on': True}})
+        cherrypy.tree.mount(advertise(self.id_droneparam), '/advertise', {'/': {'tools.gzip.on': True}})
         cherrypy.engine.start()
 
 #class getWaypoints:
@@ -35,20 +36,22 @@ class posAll():
     def __init__(self, id_droneparam):
         self.id_droneparam=id_droneparam
     @cherrypy.expose
-    def index(self):
-        json = "["
+   # @cherrypy.tools.json_out()
+    def index(self):#long vehicleID, long startID, long EndID, int percentage
+        jsonstring = '['
         a=0
         for key, value in self.id_droneparam.items():
             droneparam=self.id_droneparam.get(key)
-            json += "{\"id\":\"" + str(key) + "\","
-            json += "\"x\":\"" + str(droneparam.x) + "\","
-            json += "\"y\":\"" + str(droneparam.y) + "\","
-            json += "\"z\":\"" + str(droneparam.z) + "\"},"
+            jsonstring += "{\"vehicleID\":" + str(key) + ","
+            jsonstring += "\"startID\":" + str(droneparam.startID) + ","
+            jsonstring += "\"endID\":" + str(droneparam.endID) + ","
+            jsonstring += "\"percentage\":" + str(droneparam.percentage) + "},"
             a=1
         if a==1:
-            json=json[:-1]
-        json +="]"
-        return json
+            jsonstring=jsonstring[:-1]
+            jsonstring +=']'
+            print jsonstring
+        return json.dumps(jsonstring)
 
 class job():
     def __init__(self, id_droneparam, mqtt_client):
@@ -63,13 +66,16 @@ class job():
         return 'job'
 
 class advertise():
+    def __init__(self, id_droneparam):
+        self.id_droneparam=id_droneparam
     @cherrypy.expose
     def index(self):
         #a= int(requests.get("http://146.175.140.44:1994/bot/newBot/drone").text)#todo right adress
 
         a = randint(0, 99)
-       # if self.id_drone.get(str(a))==None:
-       #     newdroneparameters = DroneParameters()
-       #     self.id_drone[str(a)] = newdroneparameters
+        if self.id_droneparam.get(str(a))==None:
+            newdroneparameters = DroneParameters()
+            self.id_droneparam[str(a)] = newdroneparameters
+            print "new drone detect"
 
         return str(a)
--- droneparameters.py
+++ droneparameters.py
@@ -6,3 +6,6 @@ class DroneParameters:
         self.z=0
         self.job=0
         self.buzy=0
+        self.percentage=0
+        self.startID=0
+        self.endID=0
