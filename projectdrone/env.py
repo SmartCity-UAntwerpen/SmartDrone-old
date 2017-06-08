b978b7eafe0ed3b444c9aa2fb93ed4b6b21982ec
KlaasDeNys <klaas.denys@outlook.com>
2017-06-08 17:32:41 +0200

    Bugfix

--- projectdrone/env.py
+++ projectdrone/env.py
@@ -5,23 +5,22 @@ class env:
     speed_horizontal=1
     settletime=2
 
-    #addrwaypoints="http://146.175.140.44:1994/map/stringmapjson/drone"
-    addrwaypoints="http://127.0.0.1:8082/fakewaypoints"
+    addrwaypoints="http://143.129.39.151:10000/map/stringmapjson/drone"
+    #addrwaypoints="http://127.0.0.1:8082/fakewaypoints"
 
-    #addrnewid= "http://143.129.39.151:10000/bot/newBot/drone"
-    addrnewid = "http://146.175.140.44:1994/bot/newBot/drone"
-    bedugaddrnewid = True
+    addrnewid= "http://143.129.39.151:10000/bot/newBot/drone"
+    bedugaddrnewid = False
 
-    #addrkillid="http://143.129.39.151:10000/delete"
+    #addrkillid="http://143.129.39.151:10000/bot/delete"
     addrkillid = "http://146.175.140.44:1994/delete"
 
-    addradvertise="http://127.0.0.1:8082/advertise"
+    addradvertise="http://143.129.39.151:8082/advertise"
     #addradvertise="http://143.129.39.151:8082/advertise"
 
-    addrjobdone="http://146.175.140.51:8090/completeJob"
+    addrjobdone="http://143.129.39.151:8090/completeJob"
 
-    #mqttbroker="143.129.39.151"
-    mqttbroker="iot.eclipse.org"#"smartcity-ua.ddns.net"
+    mqttbroker="143.129.39.151"
+    #mqttbroker="iot.eclipse.org"
     mqttusername="root"
     mqttpassword="smartcity"
     mqttport=1883
@@ -42,7 +41,8 @@ class env:
 
     printNewPos = False
 
-    port = 'COM4'
+    #port = 'COM4'
+    port = '/dev/ttyS0'
     rate = 57600
 
     standardspeedSimulation=70
